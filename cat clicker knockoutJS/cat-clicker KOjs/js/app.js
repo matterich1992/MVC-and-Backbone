85c00d23f62fc77b042e7afaf064f9e80ac9a111
Matthew Erich <matthewerich1992@gmail.com>
2016-09-07 21:41:24 -0700

    Knockout JS cat-clicker "levels" added
    Added levels to knockoutJS app using computed observables.

--- cat clicker knockoutJS/cat-clicker KOjs/js/app.js
+++ cat clicker knockoutJS/cat-clicker KOjs/js/app.js
@@ -3,11 +3,39 @@ var ViewModel = function(){
 	this.name = ko.observable("Mittens");
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 
+	this.level = ko.computed(function(){
+		var age;
+		if(this.clickCount()<=5){
+			age = "Newborn";
+		}
+
+		else if(this.clickCount()<=10){
+			age = "Child";
+		}
+
+		else if(this.clickCount()<=20){
+			age="Teenager";	
+		}
+		else if(this.clickCount()<=25){
+			age="Confused 20's";
+		}
+
+		else{
+			age = "Adult";
+		}
+		
+		return age;
+	}, this);
+
+
+
 	this.incrementCounter = function(){
 		this.clickCount(this.clickCount()+1);
 	// same as var count = 0;
 	//	count++;
 	};
+
+
 }
 
 ko.applyBindings(new ViewModel());
